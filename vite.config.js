3daffe154626fc09ca21e9a4da6cf77febae57d8
spxce-dev <bmogolane3@gmail.com>
2026-03-12 10:05:14 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
-  base: "/",
+  base: "./",
   logLevel: "error",
   plugins: [react()],
   resolve: {
